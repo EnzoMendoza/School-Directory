89ed13fcc5414789be00a9ba4c141d4e7f020088
Enzo <emend069@fiu.edu>
2020-03-03 14:46:11 -0500

    gg

--- src/runner.java
+++ src/runner.java
@@ -3,7 +3,11 @@ public class runner {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
+		for(int i =1; i < 100; i= i+2)
+		{
+			System.out.print(i + " ");
+		}
+		
 
 	}
 
